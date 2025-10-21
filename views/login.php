06f2a54237fd6f7fab87392e56eac96606c94db9
aaron_pc <100006565.joan23@fje.edu>
2025-10-21 17:02:07 +0200

    Creacion de archivos procesar registro y procesar login

--- views/login.php
+++ views/login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-
+$errores = $_SESSION['errores'] ?? [];
+$exito = $_SESSION['exito'] ?? null;
+unset($_SESSION['errores'], $_SESSION['exito']);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,7 +18,7 @@ session_start();
         <section class="tarjeta">
             <h1>Chat Online</h1>
 
-            <form action="../procesos/login.php" method="post" autocomplete="off" class="formulario">
+            <form action="../procesos/procesar_login.php" method="post" autocomplete="off" class="formulario">
                 <label>
                     Usuario
                     <input id="username" type="text" name="username" required onblur="validaUsuario()">
@@ -30,6 +32,12 @@ session_start();
                 <button type="submit">Entrar</button>
                 <p class="texto-registrarse">¿No tienes una cuenta? <a href="registrar.php">Regístrate</a></p>
             </form>
+            <?php if ($errores): ?>
+    <div class="alerta alerta-error"><?= htmlspecialchars($errores[0], ENT_QUOTES, 'UTF-8'); ?></div>
+<?php endif; ?>
+<?php if ($exito): ?>
+    <div class="alerta alerta-exito"><?= htmlspecialchars($exito, ENT_QUOTES, 'UTF-8'); ?></div>
+<?php endif; ?>
         </section>
     </main>
 </body>
